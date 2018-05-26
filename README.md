909082a04c1843d6f4be4f2060fce99ee03222ef
walzer85 <walzer85@gmail.com>
2018-05-26 16:20:48 -0500

    Created a basic readme file and added a footer

---
+++ README.md
@@ -0,0 +1,2 @@
+This project is made for the Austin Coding Academy Course 311 Final Project.
+It was developed by Eric Walz.
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+class Footer extends Component {
+    render() {
+      return (
+        <div className="square" onClick={() => this.props.onClick()}>
+          <h4>Created by Eric Walz as part of the Austin Coding Academy Program</h4>
+        </div>
+      );
+    }
+  }
+
+export default Footer;
