a149859c96f25818e208794eb9c6b9d48668de09
walzer85 <walzer85@gmail.com>
2018-05-31 21:32:59 -0500

    working on the submission portion of the pages.  My understanding is increasing some I think

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { Component } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Tuner from "./componnets/Tuner";
 import NewUser from "./components/NewUser";
 import UserRequest from "./components/UserRequest";
 
+class App extends Comment {
+    render(){
+        return(
+            <div>
+                <Header  />
+                <Tuner />
+                <NewUser />
+                <UserRequest />
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default App;
--- src/components/UserRequest.js
+++ src/components/UserRequest.js
@@ -1,3 +1,73 @@
 import React from "react";
+import SubmitThatYou from "./SubmitThankYou";
 
-//Create the layout for people who request what they're looking for
+class UserRequest extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: this.state.name,
+            instrument: this.state.instrument,
+            genre: this.state.genre,
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        
+        this.setState({value: event.target.value});
+      }
+
+    handleSubmit(event) {
+        render() {
+            return (
+                <div>
+                    <SubmitThatYou />
+                </div>
+            )
+        }
+        
+        event.preventDefault();
+    }
+
+    render() {
+      return (
+        <div className="square">
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                Name:
+                <input
+                    name="name"
+                    type="text"
+                    value={this.state.name}
+                    onChange={this.handleInputChange} />
+                </label>
+                <br />
+                <label>
+                Instrument:
+                <input
+                    name="instrument"
+                    type="text"
+                    value={this.state.instrument}
+                    onChange={this.handleInputChange} />
+                </label>
+                <br />
+                <label>
+                Genre:
+                <input
+                    name="genres"
+                    type="text"
+                    value={this.state.genres}
+                    onChange={this.handleInputChange} />
+                </label>
+            </form>
+        </div>
+      );
+    }
+  }
+
+export default UserRequest;
